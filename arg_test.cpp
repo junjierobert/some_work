b7daaf555ffefe58ceb19d0bbddf2771758dec47
fujiyong <jiyong@ziipin.com>
2018-05-18 09:44:53 +0800

    add a arg_test file

---
+++ arg_test.cpp
@@ -0,0 +1,16 @@
+//
+//  arg_test.cpp
+//  mcmf_test
+//
+//  Created by fujiyong on 10/05/2018.
+//  Copyright © 2018 fujiyong. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+        // Check the number of parameters
+    
+}
